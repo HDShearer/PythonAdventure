7a5a0d5b5039e34a1a69e12e641b49ef2c82634d
Brendan-SB <56805977+Brendan-SB@users.noreply.github.com>
2020-01-25 14:05:51 -0500

    Added an inventory save method and a global Inventory var.

--- items.py
+++ items.py
@@ -3,26 +3,28 @@ import random
 # why'd you have to make it dictionaries?
 category = {"weapon": 0, "key": 1, 'food': 2}
 
-# name: [(type), "description", (stat)] ## stat is dependant on the type. A weapon (0) would have a damage stat. A
+# name: {(type), "description", (stat)} ## stat is dependant on the type. A weapon (0) would have a damage stat. A
 # key (1) has a stat that unlocks a lock of the corresponding value, food (2) is health restored, etc.
-items = {"sword": [0, "A sharp hunk of metal, perfect for decapitating enemies!"],
-         "key": [1, " This ornately carved piece of bronze looks perfect for opening a lock"],
+items = {"sword": [0, "A sharp hunk of metal, perfect for decapitating enemies!", 5],
+         "key": [1, " This ornately carved piece of bronze looks perfect for opening a lock", 0],
          'ration': [2, "This is a standard-issue ration. Not too tasty, but supplies you with the energy you need "
-                       "for adventuring.", 10]}
+                       "for adventuring.", 10], 'leather armour': [3, 'A simple set of armour made from cheap hide. '
+                                                                      'It does not offer much defence.', 3]}
+global Inventory
+Inventory = {0: 'test'}
 
 
 class Item(object):
+    # returns a randomItemKey object
     def generateItem(self):
-        itemsKeysAsList = None
+        randomItemKey = None
         LIST = random.choice(list(category.keys()))
         type = category[LIST]
         if type == 0:
             # attempts to generate a random item until it is a weapon
             while True:
-                itemsKeysAsList = random.choice(list(items.keys()))
-                # I used the same name for 3 different variables. This is called mental efficiency and is
-                # a good trait to have.
-                name = items[itemsKeysAsList]
+                randomItemKey = random.choice(list(items.keys()))
+                name = items[randomItemKey]
                 print(name[0])
                 if name[0] == 0:
                     break
@@ -30,51 +32,49 @@ class Item(object):
                     continue
         elif type == 1:
             while True:
-                itemsKeysAsList = random.choice(list(items.keys()))
-                name = items[itemsKeysAsList]
+                randomItemKey = random.choice(list(items.keys()))
+                name = items[randomItemKey]
                 if name[0] == 1:
                     break
                 else:
                     continue
         elif type == 2:
             while True:
-                itemsKeysAsList = random.choice(list(items.keys()))
-                name = items[itemsKeysAsList]
+                randomItemKey = random.choice(list(items.keys()))
+                name = items[randomItemKey]
                 if name[0] == 2:
                     break
                 else:
                     continue
-        return itemsKeysAsList
+        return randomItemKey
 
     def __init__(self):
         Item.generateItem(object)
 
-
-class Weapon(Item):
-    pass
-
-
-class Treasure(Item):
-    pass
-
-
-class Magic(Item):
-    pass
-
-
-class Other(Item):
-    pass
-
-
 class inventory(object):
-    def readInventory(self):
+    def startingInventory(self):
+        Inventory = {0: items['sword'], 1: items['ration'], 2: items['leather armour']}
+        return Inventory
+
+    def saveInventory(self):
+        try:
+            inventoryWriter = open('inventory.txt', 'w')
+            inventoryWriter.write(str(Inventory))
+
+        except IOError:
+            print('Error writing to inventory.txt. Type save to try again')
         return
-        inventory = dict
-        inventorySave = open('inventory.text', 'r')
-        inventoryState = inventorySave.readline()
+
+    def readInventory(self):
+        count = 0
+        inventoryLoad = open('inventory.text', 'r')
+        inventoryState = list(inventoryLoad.readlines())
         count = list(len(inventoryState))
-        for lines in inventoryState:
-            Inventory[count] = str(lines)
+        for line in inventoryState:
+            Inventory[count]: line
+            count = count + 1
+        return Inventory
 
 
-Item.__init__(object)
+# for debugging
+inventory.saveInventory(object)
