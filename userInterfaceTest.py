23ea54ef056a31c61104425b645b46c136fb89ed
Brendan-SB <56805977+Brendan-SB@users.noreply.github.com>
2020-02-06 16:42:35 -0500

    here

---
+++ userInterfaceTest.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'userInterfaceTest.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(985, 535)
+        MainWindow.setToolTip("")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(20, 20, 911, 381))
+        self.textBrowser.setObjectName("textBrowser")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 985, 26))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuEdit = QtWidgets.QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionSave_Game = QtWidgets.QAction(MainWindow)
+        self.actionSave_Game.setObjectName("actionSave_Game")
+        self.actionLoad_Game = QtWidgets.QAction(MainWindow)
+        self.actionLoad_Game.setObjectName("actionLoad_Game")
+        self.menuFile.addAction(self.actionSave_Game)
+        self.menuFile.addAction(self.actionLoad_Game)
+        self.menuFile.addSeparator()
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        self.actionSave_Game.triggered.connect(lambda: self.updateTB('save was clicked'))
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuEdit.setTitle(_translate("MainWindow", "Edit"))
+        self.actionSave_Game.setText(_translate("MainWindow", "Save Game"))
+        self.actionSave_Game.setToolTip(_translate("MainWindow", "Save Game"))
+        self.actionSave_Game.setShortcut(_translate("MainWindow", "Ctrl+S"))
+        self.actionLoad_Game.setText(_translate("MainWindow", "Load Game"))
+
+    def updateTB(self, text):
+        tb = self.textBrowser
+
+        tb.setText(text)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
