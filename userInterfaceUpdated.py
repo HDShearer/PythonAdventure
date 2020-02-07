a2e4128e281eb176d07fcfe1132bc3452c96c8a0
Brendan-SB <56805977+Brendan-SB@users.noreply.github.com>
2020-02-06 19:31:32 -0500

    made so you can preview it

--- userInterfaceUpdated.py
+++ userInterfaceUpdated.py
@@ -1,28 +1,41 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'userInterfaceTest.ui'
+# Form implementation generated from reading ui file 'userInterfaceUpdated.ui'
 #
 # Created by: PyQt5 UI code generator 5.13.0
 #
 # WARNING! All changes made in this file will be lost!
 
-
+import __main__ as game
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(985, 535)
+        MainWindow.resize(1206, 707)
         MainWindow.setToolTip("")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(10, 410, 1031, 22))
+        font = QtGui.QFont()
+        font.setFamily("Courier New")
+        font.setPointSize(9)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.lineEdit.setObjectName("lineEdit")
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(20, 20, 911, 381))
+        self.textBrowser.setEnabled(True)
+        self.textBrowser.setGeometry(QtCore.QRect(10, 10, 1031, 391))
+        font = QtGui.QFont()
+        font.setFamily("Courier New")
+        font.setPointSize(9)
+        self.textBrowser.setFont(font)
         self.textBrowser.setObjectName("textBrowser")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 985, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1206, 26))
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
@@ -36,31 +49,36 @@ class Ui_MainWindow(object):
         self.actionSave_Game.setObjectName("actionSave_Game")
         self.actionLoad_Game = QtWidgets.QAction(MainWindow)
         self.actionLoad_Game.setObjectName("actionLoad_Game")
+        self.actionEnter_current_command = QtWidgets.QAction(MainWindow)
+        self.actionEnter_current_command.setObjectName("actionEnter_current_command")
         self.menuFile.addAction(self.actionSave_Game)
         self.menuFile.addAction(self.actionLoad_Game)
         self.menuFile.addSeparator()
+        self.menuEdit.addAction(self.actionEnter_current_command)
         self.menubar.addAction(self.menuFile.menuAction())
         self.menubar.addAction(self.menuEdit.menuAction())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.actionSave_Game.triggered.connect(lambda: self.updateTB('save was clicked'))
+        self.actionEnter_current_command.triggered.connect(lambda: self.appendTB('test'))
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Type Here"))
         self.menuFile.setTitle(_translate("MainWindow", "File"))
         self.menuEdit.setTitle(_translate("MainWindow", "Edit"))
         self.actionSave_Game.setText(_translate("MainWindow", "Save Game"))
         self.actionSave_Game.setToolTip(_translate("MainWindow", "Save Game"))
         self.actionSave_Game.setShortcut(_translate("MainWindow", "Ctrl+S"))
         self.actionLoad_Game.setText(_translate("MainWindow", "Load Game"))
+        self.actionEnter_current_command.setText(_translate("MainWindow", "Enter current command"))
+        self.actionEnter_current_command.setShortcut(_translate("MainWindow", "Return"))
 
-    def updateTB(self, text):
+    def appendTB(self, text):
         tb = self.textBrowser
-
-        tb.setText(text)
+        tb.append('(current player will be here): ' + text)
 
 
 if __name__ == "__main__":
